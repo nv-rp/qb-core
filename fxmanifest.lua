ee8e51b75d6f4bee05f4fafde04f09b6426ddde6
IamMikejuuh <mikehetapestaartje@gmail.com>
2022-06-05 23:42:43 +0200

    fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ version '1.1.0'
 shared_scripts {
 	'config.lua',
     'shared/locale.lua',
-	'locale/en.lua', -- replace with desired language
+	'locale/nl.lua', -- replace with desired language
 	'shared/main.lua',
 	'shared/items.lua',
 	'shared/jobs.lua',
